2c5355187ccb6b00be993338cdc646779e51e8c2
Johnpaul <johnpaulpp05@gmail.com>
2025-06-12 18:51:27 +0530

    alen

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import Navbar from './components/Navbar'
+import Navbar from './Components/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Display from './Components/Display'
 
@@ -12,14 +12,16 @@ import Display from './Components/Display'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [category, setCategory] = useState('general'); // State to manage selected category
+  const [country, setCountry] = useState('us'); // Default country
 
   return (
     <>
 
   
-     <Navbar/>
+     <Navbar setCategory={setCategory} setCountry={setCountry}/>
      <Routes>
-     <Route path='/' element={<Display/>}></Route>
+     <Route path='/' element={<Display category={category}/>}></Route>
      
      </Routes>
 
