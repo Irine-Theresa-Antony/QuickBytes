cd4c8b933e9864844ded1b62a72c2e86bc9f5c21
Johnpaul <johnpaulpp05@gmail.com>
2025-06-12 18:53:01 +0530

    alen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,43 @@
-import { AppBar,Box, Button, IconButton, Toolbar, Typography } from '@mui/material'
-import React from 'react'
+import { AppBar,Box, Button, IconButton, Toolbar, Typography,Menu, MenuItem } from '@mui/material'
+import React, { useState } from 'react'
 import MenuIcon from '@mui/icons-material/Menu';
 
-const Navbar = () => {
+const Navbar = ({ setCategory ,setCountry}) => {
+  const [anchorEl, setAnchorEl] = useState(null); // for category button
+  const categories = ['general', 'technology', 'sports', 'health', 'business', 'science', 'entertainment'];
+
+  const [anchorCountryEl, setAnchorCountryEl] = useState(null);
+  const countries = [
+      { code: 'us', name: 'United States' },
+      { code: 'in', name: 'India' },
+      { code: 'gb', name: 'United Kingdom' },
+      { code: 'au', name: 'Australia' },
+      { code: 'ca', name: 'Canada' }
+    ];
+
+  
+  // When category button is clicked
+  const handleCategoryClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  // Close menu and optionally set category
+  const handleCategoryClose = (category) => {
+    setAnchorEl(null);
+    if (category) setCategory(category);
+  };
+
+   // Handlers for Country
+  const handleCountryClick = (event) => {
+    setAnchorCountryEl(event.currentTarget);
+  };
+
+  const handleCountryClose = (code) => {
+    setAnchorCountryEl(null);
+    if (code) setCountry(code);
+  };
+
+
   return (
     <div>
       <Box sx={{ flexGrow: 1 }}>
@@ -20,12 +55,43 @@ const Navbar = () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             QuickBytes
           </Typography>
+
+           {/*  Category Filter Button */}
+            <Button color="inherit" onClick={handleCategoryClick}>
+            Category
+            </Button>
+            <br /><br />
+          
+           {/*  Country Placeholder */}
+            <Button color="inherit" onClick={handleCountryClick}>Country</Button>
+            <br /><br />
+           {/*  Login */}
           <Button color="inherit">
             Login
-             </Button>
+          </Button>
             
         </Toolbar>
       </AppBar>
+
+        {/*  Dropdown Menu for Category */}
+         <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={() => handleCategoryClose(null)}>
+          {categories.map((cat, idx) => (
+          <MenuItem key={idx} onClick={() => handleCategoryClose(cat)}>
+           {cat.charAt(0).toUpperCase() + cat.slice(1)}
+         </MenuItem>
+              ))}
+         </Menu>
+
+
+      {/* Country Dropdown */}
+      <Menu anchorEl={anchorCountryEl} open={Boolean(anchorCountryEl)} onClose={() => handleCountryClose(null)}>
+        {countries.map((c, idx) => (
+          <MenuItem key={idx} onClick={() => handleCountryClose(c.code)}>
+            {c.name}
+          </MenuItem>
+        ))}
+      </Menu>
+
     </Box>
     </div>
   )
